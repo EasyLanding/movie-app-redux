1c0bd3aedff1657c12c2ab577a855ed60b5a04fd
Georgiy Antonevich <goshka.ruu@mail.ru>
2022-09-12 15:44:55 +0300

    add ts
    Signed-off-by: Georgiy Antonevich <goshka.ruu@mail.ru>

--- src/components/MovieContent/MovieContent.tsx
+++ src/components/MovieContent/MovieContent.tsx
@@ -25,46 +25,40 @@ import "antd/dist/antd.css";
 import { format } from 'date-fns';
 
 const pageSize = 1;
-const MovieContent = () =>
-{
-    const dispatch = useDispatch()
-    let searchDataUp = useSelector(state => state.repos.movieElement)
+const MovieContent = () => {
+    const dispatch: any = useDispatch()
+    let searchDataUp = useSelector((state: any) => state.repos.movieElement)
 
     const [idxCookie, setIdxCookie] = useState(0)
     const [minIndex, setMinIndex] = useState(0)
     const [maxIndex, setMaxIndex] = useState(0)
     const [current, setCurrent] = useState(1)
 
-    const searchMovie = ((searchData) =>
-    {
+    const searchMovie = ((searchData: string) => {
         dispatch(setLoading(true))
-        if (searchDataUp)
-        {
+        if (searchDataUp) {
             dispatch(getResponseMovieDB(searchData))
             setMinIndex(0)
             setMaxIndex(pageSize)
             dispatch(setError(false))
             dispatch(setSearchData(searchDataUp))
-            dispatch(setTotalPage((state) => state.repos.movieElement.length / pageSize))
+            dispatch(setTotalPage((state: any) => state.repos.movieElement.length / pageSize))
             dispatch(setLoading(false))
         }
-        if (searchData === '')
-        {
+        if (searchData === '') {
             dispatch(setError(true))
             dispatch(setLoading(false))
             dispatch(setMovieElement([]))
         }
     })
-    let handleChange = (page) =>
-    {
+    let handleChange = (page: number) => {
         setMinIndex((page - 1) * pageSize)
         setMaxIndex(page * pageSize)
         setCurrent(page)
     };
-    const changeRateMovie = (id) =>
-    {
+    const changeRateMovie = (id: number) => {
 
-        let idx = searchDataUp.findIndex((el) => el.id === id)
+        let idx = searchDataUp.findIndex((el: any) => el.id === id)
         let newArrString = JSON.stringify(searchDataUp[idx])
 
         let name = idx;
@@ -73,20 +67,17 @@ const MovieContent = () =>
         document.cookie = name + "=" + value
     };
 
-    const shortDescriptionMovie = (item) =>
-    {
+    const shortDescriptionMovie = (item: string) => {
         let maxLength = 28;
-        let ShortOverview = item.split(' ');
-        if (ShortOverview.length >= maxLength)
-        {
+        let ShortOverview: string[] | string = item.split(' ');
+        if (ShortOverview.length >= maxLength) {
             ShortOverview.length = maxLength;
             ShortOverview.push('...');
         }
         ShortOverview = ShortOverview.join(' ');
         return ShortOverview
     }
-    const movieRaitingNumber = (item) =>
-    {
+    const movieRaitingNumber = (item: number) => {
         let ratingColor
         if (item >= 0 && item < 3) ratingColor = ' bad';
         if (item >= 3 && item < 5) ratingColor = ' not-bad';
@@ -94,20 +85,18 @@ const MovieContent = () =>
         if (item >= 7) ratingColor = ' wonderful'
         return ratingColor
     }
-    const movieDate = (item) =>
-    {
-        if (item)
-        {
+    const movieDate = (item: any) => {
+        if (item) {
             let date = new Date(item);
             const formatDate = format(date, 'MMMM d, Y');
             let finalDate = formatDate;
             return finalDate
         }
     }
-    let errorMessage = useSelector(state => state.repos.error) ? < ErrorIndicator /> : null;
-    let length = useSelector(state => state.repos.movieElement.length)
-    let tabRated = useSelector(state => state.repos.tabRated)
-    let loading = useSelector(state => state.repos.loading)
+    let errorMessage = useSelector((state: any) => state.repos.error) ? < ErrorIndicator /> : null;
+    let length = useSelector((state: any) => state.repos.movieElement.length)
+    let tabRated = useSelector((state: any) => state.repos.tabRated)
+    let loading = useSelector((state: any) => state.repos.loading)
 
     let movieElement = searchDataUp
 
@@ -115,54 +104,53 @@ const MovieContent = () =>
         < div className='conteiner-1' >
             {
                 tabRated ? <MovieContentCookie
-                    toggleTabSearch={ () => dispatch(setTabRated(false)) }
-                    toggleTabRate={ () => dispatch(setTabRated(true)) }
-                    idxCookie={ idxCookie }
-                    changeRateMovies={ changeRateMovie }
+                    toggleTabSearch={() => dispatch(setTabRated(false))}
+                    toggleTabRated={() => dispatch(setTabRated(true))}
+                    idxCookie={idxCookie}
+                    changeRateMovies={changeRateMovie}
                 /> :
                     <div className='conteinerDiv'>
                         <div className="movieCartConteinerHeader">
                             <div className="movieCartButton">
                                 <button
                                     className='search-button'
-                                    onClick={ () => dispatch(setTabRated(false)) }
+                                    onClick={() => dispatch(setTabRated(false))}
                                 >Search</button>
                                 <button
                                     className='rate-button'
-                                    onClick={ () => dispatch(setTabRated(true)) }
+                                    onClick={() => dispatch(setTabRated(true))}
                                 >Rate</button>
                             </div>
                             <SearchInput
-                                searchMovie={ searchMovie }
+                                searchMovie={searchMovie}
                             />
                             <div className='movieCartHeader'><h1 className='movieCartHeader-1'>Movie DB</h1></div>
                         </div>
-                        { errorMessage }
+                        {errorMessage}
                         {
                             (loading) ? <Spinner /> :
                                 <div className='movieCartConteiner'>
                                     {
                                         <div className="movieCart">
                                             {
-                                                movieElement.map((itemTitle, index) =>
-                                                {
+                                                movieElement.map((itemTitle: any, index: number) => {
                                                     //логика для пагинации и отрисовка элемента movieCart render
                                                     return index >= minIndex &&
                                                         index < maxIndex && (
-                                                            <div key={ itemTitle.id } className="movieCart-cart">
-                                                                <img className='movieImg' alt={ itemTitle.id } src={ `https://image.tmdb.org/t/p/original${itemTitle.poster_path}` } />
+                                                            <div key={itemTitle.id} className="movieCart-cart">
+                                                                <img className='movieImg' alt={itemTitle.id} src={`https://image.tmdb.org/t/p/original${itemTitle.poster_path}`} />
                                                                 <div className='movieCart-cart-content'>
                                                                     <div className='movieCart-cart-content-header'>
-                                                                        <h2 className='movieHeader'>{ itemTitle.title }</h2>
-                                                                        <div className={ 'movieHeader-rate' + movieRaitingNumber(itemTitle.vote_average) }>{ itemTitle.vote_average }</div>
+                                                                        <h2 className='movieHeader'>{itemTitle.title}</h2>
+                                                                        <div className={'movieHeader-rate' + movieRaitingNumber(itemTitle.vote_average)}>{itemTitle.vote_average}</div>
                                                                     </div>
-                                                                    <div className="movie-card-date">{ movieDate(itemTitle.release_date) }</div>
-                                                                    <Genres genreIds={ itemTitle.genre_ids } />
-                                                                    <p className='movieDescription'>{ shortDescriptionMovie(itemTitle.overview) }</p>
+                                                                    <div className="movie-card-date">{movieDate(itemTitle.release_date)}</div>
+                                                                    <Genres genreIds={itemTitle.genre_ids} />
+                                                                    <p className='movieDescription'>{shortDescriptionMovie(itemTitle.overview)}</p>
                                                                     <div className='rate-movie-cart'>
                                                                         <RateM
-                                                                            id={ itemTitle.id }
-                                                                            changeRateMovies={ changeRateMovie }
+                                                                            id={itemTitle.id}
+                                                                            changeRateMovies={changeRateMovie}
                                                                         />
                                                                     </div>
                                                                 </div>
@@ -177,11 +165,11 @@ const MovieContent = () =>
                         }
                         <div className='paginationConteiner'>
                             <Pagination
-                                pageSize={ pageSize }
-                                current={ current }
-                                total={ length }
-                                onChange={ handleChange }
-                                style={ { bottom: "0px" } }
+                                pageSize={pageSize}
+                                current={current}
+                                total={length}
+                                onChange={handleChange}
+                                style={{ bottom: "0px" }}
                             />
                         </div>
                     </div>
