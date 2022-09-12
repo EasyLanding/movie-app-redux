792fd482f5f83887472183287d521ba41468f3a2
Georgiy Antonevich <goshka.ruu@mail.ru>
2022-09-12 16:02:04 +0300

    add ts
    Signed-off-by: Georgiy Antonevich <goshka.ruu@mail.ru>

--- src/redux/service.ts
+++ src/redux/service.ts
@@ -1,19 +1,15 @@
 import axios from "axios";
 import { setMovieElement, setGenre } from './reposReducer';
 
-export const getResponseMovieDB = (searchData) =>
-{
-    return async (dispatch) =>
-    {
+export const getResponseMovieDB = (searchData: any) => {
+    return async (dispatch: any) => {
         const res = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c8e44c65deebb0118bbf6902d87d51e0&language=en-US&query=${searchData}&include_adult=false`)
         const body = await res
         dispatch(setMovieElement(body.data.results))
     }
 }
-export const getResponseGenreMovieDB = () =>
-{
-    return async (dispatch) =>
-    {
+export const getResponseGenreMovieDB = () => {
+    return async (dispatch: any) => {
         const res = axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=c8e44c65deebb0118bbf6902d87d51e0`)
         const body = await res
         dispatch(setGenre(body.data.genres))
